72d09197aa01a0aab1d71e86c02f83a36d64546e
tony@foxmax.onmicrosoft.com <fowlert>
2023-09-23 17:19:06 +0100

    vnet

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
+  name                = "gitopsexample-security-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "gitopsexample-network"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
