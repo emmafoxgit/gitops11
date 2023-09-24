2d96ef0c53ca50ef2e3f962981a6290760447fb9
tony@foxmax.onmicrosoft.com <fowlert>
2023-09-24 19:30:35 +0100

    feat: sunbewt 554

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "azurerm_virtual_network" "example" {
     security_group = azurerm_network_security_group.example.id
   }
   subnet {
-    name           = "subnet54"
+    name           = "subnet554"
     address_prefix = "10.0.4.0/24"
     security_group = azurerm_network_security_group.example.id
   }
