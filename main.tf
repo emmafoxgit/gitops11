7a9b6be86ac0a30475b983db2258c04dcf86372b
tony@foxmax.onmicrosoft.com <fowlert>
2023-12-15 12:59:40 +0000

    fix:vnet2-addressrange

--- main.tf
+++ main.tf
@@ -39,13 +39,13 @@ resource "azurerm_subnet" "example_subnet" {
 
 resource "azurerm_virtual_network" "example_vnet2" {
   name                = "example-vnet2"
-  address_space       = ["10.1.1.0/16"]
+  address_space       = ["192.0.0.0/16"]
   location            = "North Europe"
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example_subnet2" {
-  for_each             = { for i in range(10) : i => format("10.1.%d.0/24", i) }
+  for_each             = { for i in range(10) : i => format("192.0.%d.0/24", i) }
   name                 = "example-subnet2-${each.key}"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example_vnet.name
