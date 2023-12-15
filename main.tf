d02864b536bd8d1e01544e5e14432c98d569fc5b
tony@foxmax.onmicrosoft.com <fowlert>
2023-12-15 10:11:23 +0000

    subnet4-gone

--- main.tf
+++ main.tf
@@ -52,12 +52,7 @@ resource "azurerm_virtual_network" "example" {
     security_group = azurerm_network_security_group.example.id
   }
   
-  subnet {
-    name           = "subnet4"
-    address_prefix = "10.0.4.0/24"
-    security_group = azurerm_network_security_group.example.id
-  }
-   
+    
   
   tags = {
     environment = "dev"
