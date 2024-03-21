2c917e90644aaca87606235a107854b1ac7bfc18
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2024-03-21 13:56:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,41 +17,6 @@ provider "azurerm" {
 
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "tonyfox-gitops-rg"
-  location = "North Europe"
-}
-
-resource "azurerm_virtual_network" "example_vnet" {
-  name                = "example-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "North Europe"
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example_subnet" {
-  for_each             = { for i in range(30) : i => format("10.0.%d.0/24", i) }
-  name                 = "example-subnet-${each.key}"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example_vnet.name
-  address_prefixes     = [each.value]
-}
-
-resource "azurerm_virtual_network" "example_vnet98982" {
-  name                = "example-vnet98982"
-  address_space       = ["192.0.0.0/16"]
-  location            = "North Europe"
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example_subnet2" {
-  for_each             = { for i in range(40) : i => format("192.0.%d.0/24", i) }
-  name                 = "example-subnet2-${each.key}"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example_vnet98982.name
-  address_prefixes     = [each.value]
-}
-
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
   source  = "Azure/naming/azurerm"
@@ -74,5 +39,3 @@ module "nsg" {
   location            = var.location
   nsgrules            = var.rules
 }
-
-
